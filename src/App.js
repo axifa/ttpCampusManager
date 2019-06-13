60ea7cf2695cf93ea0f25b74a1f97cd014395244
Asifa Khan <[asifakhan00011@gmail.com]>
2019-06-13 16:03:26 -0400

    react home

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import CampusCard from './CampusCard';
 import StudentCard from './StudentCard';
 import ShowCampus from './ShowCampus';
 import ShowStudent from './ShowStudent';
+import Home from './Home';
 
 function App() {
   return (
     <div>
-      < CampusCard campName = "Lehman College"/>
+      {/* < CampusCard campName = "Lehman College"/>
       < StudentCard name = "Alice Wang" campName ="Lehman College"/>
       < ShowCampus campName = "Lehman College"/>
-      < ShowStudent name= "Alice" gpa="3.5" campName ="Hostos"/>
+      < ShowStudent name= "Alice" gpa="3.5" campName ="Hostos"/> */}
+      <Home />
       
     </div>
   );
